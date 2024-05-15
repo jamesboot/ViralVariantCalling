d88c03def1e04a0179ea047f36b36bb0165b4987
jamesboot <james.boot.94@gmail.com>
2024-05-15 15:42:36 +0100

    Create 12-vcfR.R

---
+++ 12-vcfR.R
@@ -0,0 +1,94 @@
+# Script for analysing the output from variant calling (VCF)
+
+# Install packages
+#install.packages('vcfR')
+
+# Load packages
+library(vcfR)
+
+# Set working directory
+setwd('/Users/hmy961/Documents/projects/GC-AAA-10836/v2/')
+
+# Load vcf
+vcf <- read.vcfR('cohort_variants.vcf')
+vcf[1:4,]
+
+# Box plot of sequencing depth
+dp <- extract.gt(vcf, element='DP', as.numeric=TRUE)
+tiff(filename = 'depth.tiff',
+     width = 8,
+     height = 8,
+     units = 'in',
+     res = 300)
+par(mar=c(8,4,1,1))
+par(cex.axis=1)
+boxplot(dp, las=3, col=c("#C0C0C0", "#808080"), ylab="Depth")
+abline(h=seq(0,1e4, by=100), col="#C0C0C088")
+dev.off()
+
+# Load fasta
+dna <- ape::read.dna('/Users/hmy961/Documents/projects/GC-AAA-10836/fasta/NC_007605.fasta', format = "fasta")
+
+# Load gff
+gff <- read.table('/Users/hmy961/Documents/projects/GC-AAA-10836/fasta/NC76.gff', sep="\t", quote="")
+
+# Create chromR object
+chrom <- create.chromR(name='EBV1', vcf=vcf, seq=dna, ann=gff)
+
+# Initial plot
+tiff(filename = 'qc1.tiff',
+     width = 8,
+     height = 8,
+     units = 'in',
+     res = 300)
+plot(chrom)
+dev.off()
+
+tiff(filename = 'qc2.tiff',
+     width = 8,
+     height = 8,
+     units = 'in',
+     res = 300)
+chromoqc(chrom, dp.alpha=20)
+dev.off()
+
+# Filter and replot
+chrom <- masker(chrom, 
+                     min_QUAL = 1,
+                     min_DP = 300)
+
+# Process
+chrom <- proc.chromR(chrom, verbose=TRUE)
+tiff(filename = 'postFiltQC1.tiff',
+     width = 8,
+     height = 8,
+     units = 'in',
+     res = 300)
+plot(chrom)
+dev.off()
+
+tiff(filename = 'postFiltQC2.tiff',
+     width = 8,
+     height = 8,
+     units = 'in',
+     res = 300)
+chromoqc(chrom, dp.alpha=20)
+dev.off()
+
+# Look at sample level
+head(chrom)
+dp <- extract.gt(chrom, element="DP", as.numeric=TRUE)
+rownames(dp) <- 1:nrow(dp)
+head(dp)
+
+tiff(filename = 'heatmap.tiff',
+     width = 8,
+     height = 8,
+     units = 'in',
+     res = 300)
+heatmap.bp(dp)
+dev.off()
+
+
+
+
