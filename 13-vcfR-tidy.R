773ef6c7e4e42c5ca8a312e45e103db53bf36d12
jamesboot <james.boot.94@gmail.com>
2024-06-07 16:35:19 +0100

    Improved tile plots
    Added an if statement to the start of the script that creates an outs folder if it doesn't already exist. Then all plots are saved to this folder.
    Split the tile plots using ggplot2 facet into the two groups (MS and non-MS)

--- 13-vcfR-tidy.R
+++ 13-vcfR-tidy.R
@@ -10,6 +10,12 @@ library(tidyverse)
 # Set working directory
 setwd('/Users/hmy961/Documents/projects/GC-AAA-10836/v2/')
 
+# Create saving directory
+if (!dir.exists('vcfR-tidy_outs')) {
+  dir.create('vcfR-tidy_outs')
+}
+save.dir <- 'vcfR-tidy_outs'
+
 # Load meta
 meta <-
   readxl::read_xlsx('../SNP analysis of Saliva and sLCL geneomes.xlsx',
@@ -25,36 +31,50 @@ vcfTibble <- vcfR2tidy(vcf)
 gtTibble <- vcfTibble$gt
 
 # Summarise number of variants in all samples
-allSummary1 <- gtTibble %>%
+nVariants <- gtTibble %>%
   mutate(status = case_when(gt_DP > 0 ~ 1,
                             .default = 0)) %>%
   group_by(Indiv) %>%
   summarise(n = sum(status))
 
-colnames(allSummary1) <- c('Sample', 'nVariants')
+colnames(nVariants) <- c('Sample', 'nVariants')
 
 # Add meta data
-allSummary2 <- merge(allSummary1, meta, by = 'Sample')
+nVariants <- merge(nVariants, meta, by = 'Sample')
 
 # Box plot of nVariants for salivas
-ggplot(allSummary2[allSummary2$Type == 'Saliva', ],
+plt <- ggplot(nVariants[nVariants$Type == 'Saliva', ],
        aes(x = Group, y = nVariants)) +
   geom_boxplot() +
   geom_dotplot(binaxis = 'y',
                stackdir = 'center',
                dotsize = 0.5) +
-  labs(title = 'nVariants by Group in Saliva samples')
+  labs(title = 'n Variants by Group in Saliva samples') +
+  ylab('n Variants')
+ggsave(plt,
+       filename = paste0(save.dir, '/nVariants_saliva.tiff'),
+       height = 6,
+       width = 6,
+       units = 'in',
+       dpi = 300)
 
 # Box plot of nVariants for LCL
-ggplot(allSummary2[allSummary2$Type == 'LCL',],
+plt <- ggplot(nVariants[nVariants$Type == 'LCL',],
        aes(x = Group, y = nVariants)) +
   geom_boxplot() +
   geom_dotplot(binaxis = 'y',
                stackdir = 'center',
                dotsize = 0.5) +
-  labs(title = 'nVariants by Group in LCL samples')
+  labs(title = 'n Variants by Group in LCL samples') +
+  ylab('n Variants')
+ggsave(plt,
+       filename = paste0(save.dir, '/nVariants_LCL.tiff'),
+       height = 6,
+       width = 6,
+       units = 'in',
+       dpi = 300)
 
-# Plot all variants in LCL samples
+# Plot all variants in LCL samples as a tile plot
 tileplot <- gtTibble
 tileplot$POS <- as.factor(tileplot$POS)
 tileplot <- tileplot[grep('EBV-Mems', tileplot$Indiv), ]
@@ -68,7 +88,7 @@ plt <- ggplot(tileplot, aes(x = Indiv, y = POS)) +
         axis.text.y = element_text(size = 2))
 
 ggsave(plt,
-       filename = 'LCL_Variants.tiff',
+       filename = paste0(save.dir, '/tile_LCL.tiff'),
        height = 16,
        width = 8,
        units = 'in',
@@ -88,7 +108,7 @@ plt <- ggplot(tileplot, aes(x = Indiv, y = POS)) +
         axis.text.y = element_text(size = 2))
 
 ggsave(plt,
-       filename = 'Saliva_Variants.tiff',
+       filename = paste0(save.dir, '/tile_saliva.tiff'),
        height = 16,
        width = 8,
        units = 'in',
@@ -124,10 +144,10 @@ for (x in names(pairs)) {
   
   # Plot
   plt <- ggplot(trial, aes(x = Indiv, y = POS)) +
-    geom_tile(aes(fill = gt_GT_alleles))
+    geom_tile(aes(fill = gt_GT_alleles), colour = 'black')
   
   ggsave(plt,
-         filename = paste0(x, '_commonVariants.tiff'),
+         filename = paste0(save.dir, '/commonVariants_', x, '.tiff'),
          height = 16,
          width = 8,
          units = 'in',
@@ -143,51 +163,35 @@ tileplot <- gtTibble %>%
   filter(POS > 166483 & POS < 169088)
 tileplot$POS <- as.factor(tileplot$POS)
 tileplot <- tileplot %>%
-  mutate(gt_GT_alleles = replace(gt_GT_alleles, gt_GT_alleles == ".", NA)) 
+  mutate(gt_GT_alleles = replace(gt_GT_alleles, gt_GT_alleles == ".", NA))
+colnames(tileplot) <- c(
+  "ChromKey",
+  "POS",
+  "Sample",
+  "gt_AD",
+  "gt_DP",
+  "gt_GQ",
+  "gt_GT",
+  "gt_PL",
+  "GT_alleles"
+)
 
-plt <- ggplot(tileplot, aes(x = Indiv, y = POS)) +
-  geom_tile(aes(fill = gt_GT_alleles)) +
+# Add meta data
+tileplot <- merge(tileplot, meta, by = 'Sample')
+
+plt <- ggplot(tileplot, aes(x = Sample, y = POS)) +
+  geom_tile(aes(fill = GT_alleles), colour = 'black') +
   theme(axis.text.x = element_text(angle = 90),
-        axis.text.y = element_text(size = 10))
+        axis.text.y = element_text(size = 10)) +
+  facet_grid(. ~ Group, scales = 'free')
+
 
 ggsave(plt,
-       filename = 'LMP1_Variants.tiff',
+       filename = paste0(save.dir, '/LMP1_Variants.tiff'),
        height = 8,
        width = 8,
        units = 'in',
        dpi = 300)
-
-
-# Filter and make a new column with mutation status (0 = ref, 1 = variant)
-lmp1Summary <- gtTibble %>%
-  filter(POS > 166483 & POS < 169088) %>%
-  mutate(status = case_when(gt_DP > 0 ~ 1,
-                            .default = 0)) %>%
-  group_by(Indiv) %>%
-  summarise(n = sum(status))
-
-colnames(lmp1Summary) <- c('Sample', 'nVariants')
-
-# Add meta data
-lmp1Summary <- merge(lmp1Summary, meta, by = 'Sample')
-
-# Box plot of nVariants for salivas
-ggplot(lmp1Summary[lmp1Summary$Type == 'Saliva',],
-       aes(x = Group, y = nVariants)) +
-  geom_boxplot() +
-  geom_dotplot(binaxis = 'y',
-               stackdir = 'center',
-               dotsize = 0.5) +
-  labs(title = 'nVariants in LMP1 by Group in Saliva samples')
-
-# Box plot of nVariants for LCL
-ggplot(lmp1Summary[lmp1Summary$Type == 'LCL',],
-       aes(x = Group, y = nVariants)) +
-  geom_boxplot() +
-  geom_dotplot(binaxis = 'y',
-               stackdir = 'center',
-               dotsize = 0.5) +
-  labs(title = 'nVariants in LMP1 by Group in LCL samples')
 
 # LMP2 NC_007605.1, ID=gene-HHV4_LMP-2B, 169294	177679
 # Plot variants across samples
@@ -195,50 +199,35 @@ tileplot <- gtTibble %>%
   filter(POS > 169294 & POS < 177679)
 tileplot$POS <- as.factor(tileplot$POS)
 tileplot <- tileplot %>%
-  mutate(gt_GT_alleles = replace(gt_GT_alleles, gt_GT_alleles == ".", NA)) 
+  mutate(gt_GT_alleles = replace(gt_GT_alleles, gt_GT_alleles == ".", NA))
+colnames(tileplot) <- c(
+  "ChromKey",
+  "POS",
+  "Sample",
+  "gt_AD",
+  "gt_DP",
+  "gt_GQ",
+  "gt_GT",
+  "gt_PL",
+  "GT_alleles"
+)
 
-plt <- ggplot(tileplot, aes(x = Indiv, y = POS)) +
-  geom_tile(aes(fill = gt_GT_alleles)) +
+# Add meta data
+tileplot <- merge(tileplot, meta, by = 'Sample')
+
+plt <- ggplot(tileplot, aes(x = Sample, y = POS)) +
+  geom_tile(aes(fill = GT_alleles), colour = 'black') +
   theme(axis.text.x = element_text(angle = 90),
-        axis.text.y = element_text(size = 10))
+        axis.text.y = element_text(size = 10)) +
+  facet_grid(. ~ Group, scales = 'free')
 
 ggsave(plt,
-       filename = 'LMP2_Variants.tiff',
+       filename = paste0(save.dir, '/LMP2_Variants.tiff'),
        height = 8,
        width = 8,
        units = 'in',
        dpi = 300)
 
-# Filter and make a new column with mutation status (0 = ref, 1 = variant)
-lmp2Summary <- gtTibble %>%
-  filter(POS > 169294 & POS < 177679) %>%
-  mutate(status = case_when(gt_DP > 0 ~ 1,
-                            .default = 0)) %>%
-  group_by(Indiv) %>%
-  summarise(n = sum(status))
-
-colnames(lmp2Summary) <- c('Sample', 'nVariants')
-
-# Add meta data
-lmp2Summary <- merge(lmp2Summary, meta, by = 'Sample')
-
-# Box plot of nVariants for salivas
-ggplot(lmp2Summary[lmp2Summary$Type == 'Saliva',],
-       aes(x = Group, y = nVariants)) +
-  geom_boxplot() +
-  geom_dotplot(binaxis = 'y',
-               stackdir = 'center',
-               dotsize = 0.5) +
-  labs(title = 'nVariants in LMP2 by Group in Saliva samples')
-
-# Box plot of nVariants for LCL
-ggplot(lmp2Summary[lmp2Summary$Type == 'LCL',],
-       aes(x = Group, y = nVariants)) +
-  geom_boxplot() +
-  geom_dotplot(binaxis = 'y',
-               stackdir = 'center',
-               dotsize = 0.5) +
-  labs(title = 'nVariants in LMP2 by Group in LCL samples')
 
 
 
